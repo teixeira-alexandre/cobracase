f8263143fb5cec63343c284fc0fe9f7088d2a057
teixeira-alexandre <alexandre1997teixeira@gmail.com>
2024-06-09 04:29:02 -0300

    finish summary and start payment section

--- src/app/configure/preview/DesignPreview.tsx
+++ src/app/configure/preview/DesignPreview.tsx
@@ -1,10 +1,13 @@
 "use client";
 
 import Phone from "@/components/Phone";
-import { cn } from "@/lib/utils";
-import { COLORS, MODELS } from "@/validators/option-validator";
+import { Button } from "@/components/ui/button";
+import { BASE_PRICE, PRODUCT_PRICES } from "@/config/products";
+import { cn, formatPrice } from "@/lib/utils";
+import { COLORS, FINISHES, MODELS } from "@/validators/option-validator";
 import { Configuration } from "@prisma/client";
-import { Check } from "lucide-react";
+import { useMutation } from "@tanstack/react-query";
+import { ArrowRight, Check } from "lucide-react";
 import { useRouter } from "next/navigation";
 import React, { useEffect, useState } from "react";
 import Confetti from "react-dom-confetti";
@@ -12,12 +15,27 @@ import Confetti from "react-dom-confetti";
 const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
   const router = useRouter();
   const [showConfetti, setShowConfetti] = useState<boolean>(false);
-  useEffect(() => setShowConfetti(true));
-  const { color, model } = configuration;
-  const {label: modelLabel} = MODELS.options.find(({value}) => value === model)!
+  useEffect!(() => setShowConfetti(true));
+  const { color, model, finish, material } = configuration;
+  const { label: modelLabel } = MODELS.options.find(
+    ({ value }) => value === model
+  )!;
   const tw = COLORS.find(
     (supportedColor) => supportedColor.value === color
   )?.tw;
+
+  let totalPrice = BASE_PRICE;
+  if (material === "polycarbonate") {
+    totalPrice += PRODUCT_PRICES.material.polycarbonate;
+  }
+  if (finish === "textured") {
+    totalPrice += PRODUCT_PRICES.finish.textured;
+  }
+
+  const {} = useMutation({
+    mutationKey: ["get-checkout-session"],
+    // mutationFn: 
+  })
 
   return (
     <>
@@ -42,13 +60,73 @@ const DesignPreview = ({ configuration }: { configuration: Configuration }) => {
             Your {modelLabel} Case
           </h3>
           <div className="mt-3 flex items-center gap-1.5 text-base">
-            <Check className="h-4 w-4 text-green-500"/>
+            <Check className="h-4 w-4 text-green-500" />
             In stock and ready to ship!
           </div>
         </div>
         <div className="sm:colspan-12 md:col-span-9 text-base">
           <div>
-            
+            <div className="grid grid-cols-1 gap-y-8 border-b border-gray-200 py-8 sm:grid-cols-2 sm:gap-x-6 sm:py-6 md:py-10">
+              <div>
+                <p className="font-medium text-zinc-950">Highlights</p>
+                <ol className="mt-3 text-zinc-700 list-disc list-inside">
+                  <li>Wireless charging compatible</li>
+                  <li>Shock resistance</li>
+                  <li>Made with recycled materials</li>
+                  <li>5 year print warranty</li>
+                </ol>
+              </div>
+              <div>
+                <p className="font-medium text-zinc-950">Materials</p>
+                <ol className="mt-3 text-zinc-700 list-disc list-inside">
+                  <li>High durability material</li>
+                  <li>Scratch-proof</li>
+                </ol>
+              </div>
+            </div>
+            <div className="mt-8">
+              <div className="bg-gray-50 p-6 sm:rounded-lg sm:p-8">
+                <div className="flow-root text-sm">
+                  <div className="flex items-center justify-between py-1 mt-2">
+                    <p className="text-gray-600">Base Price</p>
+                    <p className="font-medium text-gray-900">
+                      {formatPrice(BASE_PRICE / 100)}
+                    </p>
+                  </div>
+                  {finish === "textured" ? (
+                    <div className="flex items-center justify-between py-1 mt-2">
+                      <p className="text-gray-600">Textured finish</p>
+                      <p className="font-medium text-gray-900">
+                        {formatPrice(PRODUCT_PRICES.finish.textured / 100)}
+                      </p>
+                    </div>
+                  ) : null}
+                  {material === "polycarbonate" ? (
+                    <div className="flex items-center justify-between py-1 mt-2">
+                      <p className="text-gray-600">
+                        Soft Polycarbonate Material
+                      </p>
+                      <p className="font-medium text-gray-900">
+                        {formatPrice(
+                          PRODUCT_PRICES.material.polycarbonate / 100
+                        )}
+                      </p>
+                    </div>
+                  ) : null}
+
+                  <div className="my-2 h-px bg-gray-200" />
+                  <div className="flex items-center justify-between py-2">
+                    <p className="font-semibold text-gray-900">
+                      Order total price
+                    </p>
+                    <p className="font-semibold text-gray-900">{formatPrice(totalPrice / 100)}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mt-8 flex justify-end pb-12">
+                  <Button disabled={true} isLoading={true} loadingText="loading" className="px-4 sm:px-6 lg:px-8">Check Out <ArrowRight className="h-4 w-4 ml-1.5 inline"/></Button>
+            </div>
           </div>
         </div>
       </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import Footer from "@/components/Footer";
 import { Toaster } from "@/components/ui/toaster";
 import Providers from "@/components/Providers";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter ({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -22,7 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <NavBar />
-        <main className="flex flex-col min-h-[calc(100vh-3.5rem-1px)]">
+        <main className="flex flex-col grainy-light min-h-[calc(100vh-3.5rem-1px)]">
           <div className="flex-1 flex flex-col h-full"><Providers>{children}</Providers></div>
           <Footer />
         </main>
